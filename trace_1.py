04e4f99549858947521f247329c4cd76fcc10db0
Alice Nelson <atnelson8@wisc.edu>
2024-12-12 23:02:59 -0600

    renamed trace.py

---
+++ trace_1.py
@@ -0,0 +1,19 @@
+import argparse
+import socket
+import struct
+import time
+
+host_name = socket.gethostname()
+address = socket.gethostbyname(host_name)
+sock = socket.socket(type = socket.SOCK_DGRAM)
+sock.setblocking(False)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description = "trace a route")
+
+    parser.add_argument("-a", required = True)
+    parser.add_argument("-b", required = True)
+    parser.add_argument("-c", required = True)
+    parser.add_argument("-d", required = True)
+    parser.add_argument("-e", required = True)
+    parser.add_argument("-f", required = True)
