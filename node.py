100086ba45ffddff89693f6b24d36571096b5eb2
Alice Nelson <atnelson8@wisc.edu>
2025-03-26 00:24:46 -0500

    added comments to node.py

--- node.py
+++ node.py
@@ -1,16 +1,22 @@
 import struct
 import socket
 
+# A class representing a node in the network,
+# defined by the node's ip and port
 
+class node:
 
-class node: 
+    # These 2 functions convert between an ip address represented with a string 
+    # formatted as "255.255.255.255" and an ip address represented as an integer
     @classmethod
-    def ip_to_int(addr):
+    def ip_to_int(self, addr):
         return struct.unpack("!I", socket.inet_aton(addr))[0]
 
     @classmethod
-    def int_to_ip(addr):
+    def int_to_ip(self, addr):
         return socket.inet_ntoa(struct.pack("!I", addr))
+    
+
     
     def __init__(self, ip, port):
         self.ip     = ip
@@ -18,12 +24,11 @@ class node:
         self.ip_num = node.ip_to_int(ip)
         self.pair   = (ip, port)
 
+    # initialize a new node from a tuple with the format ("255.255.255.255", "1")
     @classmethod
     def from_str_pair(self, node_pair):
         return node(node_pair[0], int(node_pair[1]))
     
-
-
     def __eq__(self, node2):
         return self.ip_num == node2.ip_num and self.port == node2.port
     
